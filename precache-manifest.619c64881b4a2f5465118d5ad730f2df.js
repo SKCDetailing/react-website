c2ce6b4e23708180fa7a5ccc6e9f78cd243bce62
SKCDetailing <135679310+SKCDetailing@users.noreply.github.com>
2023-09-07 22:23:46 -0700

    Move website code to root directory

---
+++ precache-manifest.619c64881b4a2f5465118d5ad730f2df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d708369a101ed0584e78",
+    "url": "/static/css/main.b732c19c.chunk.css"
+  },
+  {
+    "revision": "d708369a101ed0584e78",
+    "url": "/static/js/main.d708369a.chunk.js"
+  },
+  {
+    "revision": "434376a2772065e17f47",
+    "url": "/static/js/1.434376a2.chunk.js"
+  },
+  {
+    "revision": "378ccf4469d78c1c9f61",
+    "url": "/static/css/2.f0cccbc6.chunk.css"
+  },
+  {
+    "revision": "378ccf4469d78c1c9f61",
+    "url": "/static/js/2.378ccf44.chunk.js"
+  },
+  {
+    "revision": "16b224df1b2d37d45cf5",
+    "url": "/static/js/runtime~main.16b224df.js"
+  },
+  {
+    "revision": "ce41d0e10262a275509fda1d6fa6f349",
+    "url": "/static/media/logo.ce41d0e1.png"
+  },
+  {
+    "revision": "7e8fbfb295504ceec0c4644dd2ed560b",
+    "url": "/index.html"
+  }
+];
